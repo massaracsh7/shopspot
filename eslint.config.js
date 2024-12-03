3f0860e0693bf41c84a97f1d99a4d8f4ef91b40a
massaracsh7 <fatum7@gmail.com>
2024-12-03 22:31:50 +0300

    feat: add prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,88 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import * as tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import prettier from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+import react from 'eslint-plugin-react';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default [
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    ignores: [
+      'vite.config.ts',
+      'node_modules',
+      'dist',
+      'package.json',
+      'package-lock.json',
+      'tsconfig.json',
+      'tsconfig.node.json',
+      '.prettierrc',
+      'prettify.js',
+      'typings.d.ts',
+      'fileMock.ts',
+    ],
+  },
+
+  js.configs.recommended,
+
+  {
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+        React: 'readonly', 
+      },
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+        ecmaFeatures: {
+          jsx: true, 
+        },
+      },
     },
     plugins: {
+      '@typescript-eslint': tsPlugin,
+      react,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off', 
+      '@typescript-eslint/explicit-module-boundary-types': 'warn',
+    },
+  },
+
+  {
+    plugins: {
       'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+    },
+  },
+
+  {
+    plugins: {
+      'react-refresh': reactRefresh,
+    },
+    rules: {
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
     },
   },
-)
+
+  {
+    plugins: {
+      prettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
+
+  prettierConfig,
+];
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler' // or "modern"
+      }
+    }
+  },
+  //base: '/pokedex',
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
 })
