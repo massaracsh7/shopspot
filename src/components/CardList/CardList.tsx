87fdd7aa7ef385d668f0855e8fc4a76502aa192b
massaracsh7 <fatum7@gmail.com>
2024-12-04 23:31:08 +0300

    feat: add pagination

--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -15,23 +15,60 @@ const CardsList: React.FC = () => {
   const favorites = products.filter((item) => favoriteProductIds.some((id) => id === item.id));
   const filteredProducts = showFavorites ? favorites : products || [];
 
+
+  const [page, setPage] = useState(1); 
+  const limit = 5; 
+
+  const startIndex = (page - 1) * limit;
+  const endIndex = startIndex + limit;
+  const paginatedProducts = filteredProducts?.slice(startIndex, endIndex);
+
+  const handleNextPage = () => {
+    if (filteredProducts && page < Math.ceil(filteredProducts.length / limit)) {
+      setPage(page + 1);
+    }
+  };
+
+  const handlePrevPage = () => {
+    if (page > 1) {
+      setPage(page - 1);
+    }
+  };
+
+  const handleFilter = (show: boolean) => {
+    setPage(1);
+    setShowFavorites(show);
+  }
+
   return (
     <div>
       {error && <p>Error loading  data</p>}
       {isLoading && <p>Loading ...</p>}
       <div className="filter">
-        <button onClick={() => setShowFavorites(false)} disabled={!showFavorites}>
+        <button onClick={() => handleFilter(false)} disabled={!showFavorites}>
           Show All
         </button>
-        <button onClick={() => setShowFavorites(true)} disabled={showFavorites}>
+        <button onClick={() => handleFilter(true)} disabled={showFavorites}>
           Show Favorites
         </button>
       </div>
       <ul className="product-list">
-        {filteredProducts.map((product: Product) => (
+        {paginatedProducts?.map((product: Product) => (
           <CardItem product={product} key={product.id} />
         ))}
       </ul>
+      <div className="pagination">
+        <button onClick={handlePrevPage} disabled={page === 1}>
+          Previous
+        </button>
+        <span>Page {page}</span>
+        <button
+          onClick={handleNextPage}
+          disabled={page >= Math.ceil(products.length / limit)}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 };
